c2cd31d913f3e67e9ef2c91f9b7b08167aa179e7
ger619 <draker74@gmail.com>
2024-05-22 19:19:49 +0300

    adding shopping cart to the app and installing turbo

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,5 @@ class Product < ApplicationRecord
   has_one_attached :image
   has_many :quantities, foreign_key: :product_id, class_name: 'Quantity', dependent: :destroy
 
-
   # has_and_belongs_to_many that joins product and quantity
 end
